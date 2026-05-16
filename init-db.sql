e4f9adfc4e0deb82b54035ed088819de6d19f34d
Aditya Singh <adityasingh4082002@gmail.com>
2026-05-16 07:41:59 +0530

    [Aditya Singh] Fix. init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -10,6 +10,9 @@ CREATE DATABASE IF NOT EXISTS airnexus_passenger CHARACTER SET utf8mb4 COLLATE u
 CREATE DATABASE IF NOT EXISTS airnexus_payment CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS airnexus_notification CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE USER IF NOT EXISTS 'airnexus_user'@'%' IDENTIFIED BY 'system';
+
+
 -- Grant all privileges to the app user on all airnexus databases
 GRANT ALL PRIVILEGES ON airnexus_auth.* TO 'airnexus_user'@'%';
 GRANT ALL PRIVILEGES ON airnexus_airline.* TO 'airnexus_user'@'%';
